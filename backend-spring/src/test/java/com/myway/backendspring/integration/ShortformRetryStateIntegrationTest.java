ebc733bc821d24247964423abb652899bb053fcf
shsh99 <ggg9905@naver.com>
2026-04-30 18:41:30 +0900

    fix: remove UTF-8 BOM from retry state integration test

--- backend-spring/src/test/java/com/myway/backendspring/integration/ShortformRetryStateIntegrationTest.java
+++ backend-spring/src/test/java/com/myway/backendspring/integration/ShortformRetryStateIntegrationTest.java
@@ -1,4 +1,4 @@
-﻿package com.myway.backendspring.integration;
+package com.myway.backendspring.integration;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
