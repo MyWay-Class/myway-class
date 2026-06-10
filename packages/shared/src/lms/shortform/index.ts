c64927996f174aae88c661eaa20d2f0b6104973c
shsh99 <ggg9905@naver.com>
2026-06-10 16:41:29 +0900

    fix: let shortform compose use timeline clips

--- packages/shared/src/lms/shortform/index.ts
+++ packages/shared/src/lms/shortform/index.ts
@@ -1,4 +1,5 @@
 export { generateShortformExtraction, listShortformCandidates, toggleShortformCandidateSelection, composeShortformVideo, getShortformExtractionById } from './compose';
 export { shareShortformVideo, listShortformCommunity, saveShortformVideo, toggleShortformLike } from './community';
 export { listMyShortformVideos, listMyShortformLibrary, getShortformVideoDetail } from './library';
+export { createShortformVideoFromCandidates } from './helpers';
 export { updateVideoExport, normalizeShortformDescription } from './data';
